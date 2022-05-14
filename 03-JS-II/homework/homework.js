ca9b217892d8670de2fde71176e3871d9abb1361
Facundo Gomez <fgfacundogomez@hotmail.com>
2022-05-14 14:49:11 -0300

    NotEnought for doWhile, tablaDelseis and esPrimo, keep going

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -180,9 +180,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-  let seis = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]
-console.log(decadas[0])  
-  
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
+  }
+  return arrayTablaDel6
 }
 
 function tieneTresDigitos(numero){
@@ -199,7 +201,15 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-}
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while(i < 8);
+  return a;
+} 
 
 
 // No modificar nada debajo de esta línea
